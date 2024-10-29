b7c6f7eb7764bc75b589a9b0072d7bb1496ed995
Nguyễn Văn Trung <nguyenvantrung4514@gmail.com>
2024-10-29 07:00:05 +0700

    Update B4.py

--- OpenCV/B4.py
+++ OpenCV/B4.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 img = cv2.imread('Pic1.jpg')
+img = cv2.resize(img, (300, 300))
 
 kernel_identity = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
 kernel_3x3 = np.ones((3, 3), np.float32) / 9.0
@@ -14,18 +15,20 @@ img_copy = img.copy()
 output_5x5 = cv2.filter2D(img[0:100, 0:100], -1, kernel_5x5)
 img_copy[0:100, 0:100] = output_5x5
 
-def add_text(image, text):
-    return cv2.putText(image.copy(), text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+def create_captioned_image(image, caption, width, height):
+    caption_area = np.zeros((50, width, 3), dtype=np.uint8)
+    cv2.putText(caption_area, caption, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+    return cv2.vconcat([caption_area, image])
 
-img_annotated = add_text(img, "Original")
-output_identity_annotated = add_text(output_identity, "Identity Filter")
-output_3x3_annotated = add_text(output_3x3, "3x3 Filter")
-output_5x5_annotated = add_text(img_copy, "5x5 Filter")
+img_captioned = create_captioned_image(img, "Original", 300, 300)
+output_identity_captioned = create_captioned_image(output_identity, "Identity Filter", 300, 300)
+output_3x3_captioned = create_captioned_image(output_3x3, "3x3 Filter", 300, 300)
+output_5x5_captioned = create_captioned_image(img_copy, "5x5 Filter", 300, 300)
 
-top_row = cv2.hconcat([img_annotated, output_identity_annotated])
-bottom_row = cv2.hconcat([output_3x3_annotated, output_5x5_annotated])
+top_row = cv2.hconcat([img_captioned, output_identity_captioned])
+bottom_row = cv2.hconcat([output_3x3_captioned, output_5x5_captioned])
 combined = cv2.vconcat([top_row, bottom_row])
 
-cv2.imshow('Filters Combined in Grid', combined)
+cv2.imshow('Filters Combined with Captions', combined)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
